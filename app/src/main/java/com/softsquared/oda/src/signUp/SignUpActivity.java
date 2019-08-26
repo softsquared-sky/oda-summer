b0f299044aabc8a5f76756a5932a5af36b416c8e
softsquared-summer <54467138+softsquared-summer@users.noreply.github.com>
2019-08-26 20:30:30 +0900

    스플래쉬_로그인_회원가입_0.0.0(체크박스 및 커스텀 버튼 미구현)

---
+++ app/src/main/java/com/softsquared/oda/src/signUp/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.softsquared.oda.src.signUp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.softsquared.odaproject.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
